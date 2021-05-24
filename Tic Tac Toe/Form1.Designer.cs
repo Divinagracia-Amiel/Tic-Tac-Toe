702bd638f2440d1a9085fd3322dd86473c024ae0
Amiel Osias Divinagracia <amielosiasddivinagracia@iskolarngbayan.pup.edu.ph>
2021-05-25 01:25:49 +0800

    Resized buttons

--- Tic Tac Toe/Form1.Designer.cs
+++ Tic Tac Toe/Form1.Designer.cs
@@ -45,87 +45,87 @@ namespace Tic_Tac_Toe
             this.tp_lft.Location = new System.Drawing.Point(12, 12);
             this.tp_lft.Margin = new System.Windows.Forms.Padding(0);
             this.tp_lft.Name = "tp_lft";
-            this.tp_lft.Size = new System.Drawing.Size(100, 100);
+            this.tp_lft.Size = new System.Drawing.Size(125, 125);
             this.tp_lft.TabIndex = 0;
             this.tp_lft.UseVisualStyleBackColor = true;
             this.tp_lft.Click += new System.EventHandler(this.tp_lft_Click);
             // 
             // tp
             // 
-            this.tp.Location = new System.Drawing.Point(112, 12);
+            this.tp.Location = new System.Drawing.Point(137, 12);
             this.tp.Margin = new System.Windows.Forms.Padding(0);
             this.tp.Name = "tp";
-            this.tp.Size = new System.Drawing.Size(100, 100);
+            this.tp.Size = new System.Drawing.Size(125, 125);
             this.tp.TabIndex = 1;
             this.tp.UseVisualStyleBackColor = true;
             this.tp.Click += new System.EventHandler(this.tp_Click);
             // 
             // tp_rt
             // 
-            this.tp_rt.Location = new System.Drawing.Point(212, 12);
+            this.tp_rt.Location = new System.Drawing.Point(262, 12);
             this.tp_rt.Margin = new System.Windows.Forms.Padding(0);
             this.tp_rt.Name = "tp_rt";
-            this.tp_rt.Size = new System.Drawing.Size(100, 100);
+            this.tp_rt.Size = new System.Drawing.Size(125, 125);
             this.tp_rt.TabIndex = 2;
             this.tp_rt.UseVisualStyleBackColor = true;
             this.tp_rt.Click += new System.EventHandler(this.tp_rt_Click);
             // 
             // lft
             // 
-            this.lft.Location = new System.Drawing.Point(12, 112);
+            this.lft.Location = new System.Drawing.Point(137, 262);
             this.lft.Margin = new System.Windows.Forms.Padding(0);
             this.lft.Name = "lft";
-            this.lft.Size = new System.Drawing.Size(100, 100);
+            this.lft.Size = new System.Drawing.Size(125, 125);
             this.lft.TabIndex = 3;
             this.lft.UseVisualStyleBackColor = true;
             this.lft.Click += new System.EventHandler(this.lft_Click);
             // 
             // mid
             // 
-            this.mid.Location = new System.Drawing.Point(112, 112);
+            this.mid.Location = new System.Drawing.Point(262, 137);
             this.mid.Margin = new System.Windows.Forms.Padding(0);
             this.mid.Name = "mid";
-            this.mid.Size = new System.Drawing.Size(100, 100);
+            this.mid.Size = new System.Drawing.Size(125, 125);
             this.mid.TabIndex = 4;
             this.mid.UseVisualStyleBackColor = true;
             this.mid.Click += new System.EventHandler(this.mid_Click);
             // 
             // rt
             // 
-            this.rt.Location = new System.Drawing.Point(212, 112);
+            this.rt.Location = new System.Drawing.Point(12, 262);
             this.rt.Margin = new System.Windows.Forms.Padding(0);
             this.rt.Name = "rt";
-            this.rt.Size = new System.Drawing.Size(100, 100);
+            this.rt.Size = new System.Drawing.Size(125, 125);
             this.rt.TabIndex = 5;
             this.rt.UseVisualStyleBackColor = true;
             this.rt.Click += new System.EventHandler(this.rt_Click);
             // 
             // btm_lft
             // 
-            this.btm_lft.Location = new System.Drawing.Point(12, 212);
+            this.btm_lft.Location = new System.Drawing.Point(12, 137);
             this.btm_lft.Margin = new System.Windows.Forms.Padding(0);
             this.btm_lft.Name = "btm_lft";
-            this.btm_lft.Size = new System.Drawing.Size(100, 100);
+            this.btm_lft.Size = new System.Drawing.Size(125, 125);
             this.btm_lft.TabIndex = 6;
             this.btm_lft.UseVisualStyleBackColor = true;
             this.btm_lft.Click += new System.EventHandler(this.btm_lft_Click);
             // 
             // btm
             // 
-            this.btm.Location = new System.Drawing.Point(112, 212);
+            this.btm.Location = new System.Drawing.Point(262, 262);
             this.btm.Margin = new System.Windows.Forms.Padding(0);
             this.btm.Name = "btm";
-            this.btm.Size = new System.Drawing.Size(100, 100);
+            this.btm.Size = new System.Drawing.Size(125, 125);
             this.btm.TabIndex = 7;
             this.btm.UseVisualStyleBackColor = true;
             this.btm.Click += new System.EventHandler(this.btm_Click);
             // 
             // btm_rt
             // 
-            this.btm_rt.Location = new System.Drawing.Point(212, 212);
+            this.btm_rt.Location = new System.Drawing.Point(137, 137);
             this.btm_rt.Margin = new System.Windows.Forms.Padding(0);
             this.btm_rt.Name = "btm_rt";
-            this.btm_rt.Size = new System.Drawing.Size(100, 100);
+            this.btm_rt.Size = new System.Drawing.Size(125, 125);
             this.btm_rt.TabIndex = 8;
             this.btm_rt.UseVisualStyleBackColor = true;
             this.btm_rt.Click += new System.EventHandler(this.btm_rt_Click);
